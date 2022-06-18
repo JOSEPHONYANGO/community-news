e29b2bd63043b08e340fca179de09ee8fdbf03cd
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-18 14:05:43 +0300

    location

--- News/models.py
+++ News/models.py
@@ -9,6 +9,32 @@ from django.db.models.signals import post_save
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # save location
+    def save_category(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # save location
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name        
+
+
+
 
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=100)
