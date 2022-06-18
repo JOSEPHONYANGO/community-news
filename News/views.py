5327b782db0ae6e59108d6d4dc9bcee0ada134da
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-18 18:49:32 +0300

    views

--- News/views.py
+++ News/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from .forms import *
+from .models import *
 
 # Create your views here.
+
+
