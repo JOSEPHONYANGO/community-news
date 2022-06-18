cc9df7900faf7043c5e5124a134517b50ca9ebae
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-18 11:21:30 +0300

    forms two

--- News/models.py
+++ News/models.py
@@ -7,3 +7,14 @@ class Neighbourhood(models.Model):
     location = models.CharField(max_length=100)
     population = models.IntegerField(max_length=50)
     admin = models.ForeignKey(admin,on_delete=models.CASCADE)
+
+class User (models.Model):
+    name = models.CharField(max_length=100)
+    id = models.AutoField(max_length=100) 
+    Neighbourhood = models.ForeignKey(Neighbourhood)
+    Email = models.EmailField(max_length=100)
+
+class Business(models.Model):
+    Business_name = models.CharField(max_length=100)
+    User = models.ForeignKey(Business,on_delete=models.CASCADE)
+    Neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)       
