45c18cf223f531712f02dcb8f16a5dfeff9d6e69
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-18 18:55:04 +0300

    views two

--- News/views.py
+++ News/views.py
@@ -8,4 +8,55 @@ from .models import *
 
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
+def home(request):
+    current_user = request.user
+    profiles = Profile.objects.filter(user_id = current_user.id).all()
+    post =  Post.objects.all().order_by('-id')
 
+
+    return render(request, 'home.html',{'profiles':profiles, 'post':post})
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    form = ProfileForm(request.POST, request.FILES)
+    if request.method == 'POST':  
+        
+        
+        if form.is_valid():
+            prof = form.save(commit=False)
+            prof.user = request.user
+            prof.save()
+            return redirect ('index')
+        
+        else:
+            form = ProfileForm()
+    return render(request, 'profile_form.html', {'form': form})
+
+
+
+@login_required(login_url="/accounts/login/")
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    profiles = Profile.objects.filter(user_id = current_user.id).all()
+
+    ctx = {"profile": profile, 'profiles':profiles}
+    return render(request, "profile.html", ctx)
+
+@login_required(login_url="/accounts/login/")
+def update_profile(request,id):
+    user = User.objects.get(id=id)
+    profile = Profile.objects.get(user = user)
+    form = UpdateProfileForm(instance=profile)
+    if request.method == "POST":
+            form = UpdateProfileForm(request.POST,request.FILES,instance=profile)
+            if form.is_valid():  
+                
+                profile = form.save(commit=False)
+                profile.save()
+                return redirect('profile') 
+            
+    ctx = {"form":form}
+    return render(request, 'update_profile.html', ctx)
