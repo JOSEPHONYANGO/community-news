276912396ab59f393ffea3adc67e5451ee0f85f8
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-18 11:06:07 +0300

    second models

--- News/forms.py
+++ News/forms.py
@@ -9,4 +9,19 @@ class CreateNeighbourhoodForm(forms.Model):
         model = Neighbourhood
         fields = ['name',]
 
-class        
+class DeleteNeighbourhoodForm(forms.Model):
+    class Meta:
+        model = Neighbourhood
+        fields = ['name']
+
+class FindNeighbourhoodForm(forms.Model):
+    class Meta:
+        model =         
+
+class UpdateNeighbourhoodForm(forms.Model):
+    class Meta:
+        model = 
+
+class UpdateOccupants(forms.Model):
+    class Meta:
+        model =         
