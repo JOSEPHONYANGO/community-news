9aed2d8a4c1e87cbcd280ba11606f28a436ddb9b
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-20 05:32:28 +0300

    images

--- CommunityNews/settings.py
+++ CommunityNews/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +89,12 @@ DATABASES = DATABASES = {
     'PASSWORD':'Access',
     }
 }
+
+cloudinary.config(
+   cloud_name = 'bishopjoe',
+   api_key = '535635979348921',
+   api_secret = 'z5jAymBVmxJHFFt5E8ETWvYEkQM' 
+) 
 
 
 # Password validation
